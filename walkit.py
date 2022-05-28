86089043d212c42f57aee56cbc5bc103ae9e1d34
k1-k0 <kmssrvk@gmail.com>
2022-05-28 12:35:39 +0300

    define db methods and add base methods for work

--- walkit.py
+++ walkit.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import Optional
+from typing import Optional, Tuple, List
 import datetime as dt
 
 from utils import get_database_path
@@ -14,25 +14,60 @@ class Database:
         self.connection.commit()
         self.connection.close()
 
-    def prepare_table(self):
+    def _prepare_table(self):
+        # TODO add check for table existence 
         cur = self.connection.cursor()
         cur.execute("create table walks (date TIMESTAMP, metres INTEGER)")
+
         cur.close()
         self.connection.commit()
 
-    def add_record(self, metres: int = 0, date: Optional[dt.datetime] = None):
+    def insert(self, sql: str, values: Optional[Tuple] = None):
+        cur = self.connection.cursor()
+
+        cur.execute(sql) if not values else cur.execute(sql, values)
+        
+        cur.close()
+        self.connection.commit()
+
+    def select(self, sql: str, values: Optional[Tuple] = None) -> List:
+        cur = self.connection.cursor()
+
+        cur.execute(sql) if not values else cur.execute(sql, values)
+
+        items = cur.fetchall()
+        cur.close()
+
+        return items
+
+    def delete(self, sql: str) -> None:
+        cur = self.connection.cursor()
+        
+        cur.execute(sql)
+        
+        cur.close()
+        self.connection.commit()
+
+
+    # TODO: this and below method get out to separate class,
+    # which will work with business logic (add/remove/list records)
+
+    def add_record(self, metres: int = 0, 
+                   date: Optional[dt.datetime] = None) -> None:
         cur = self.connection.cursor()
 
         if not date:
             date = dt.datetime.now().date()
 
         # TODO: Options if date already exists in table: Rewrite, Append, Nothing
-        cur.execute("insert into walks values (?, ?)", (date, metres))
+        query = "insert into walks values (?, ?)"
+        self.insert(sql=query, values=(date, metres))
 
-        cur.close()
-        self.connection.commit()
-
-    def get_records(self):
-        cur = self.connection.cursor()
-        cur.execute("select * from walks")
-        print(cur.fetchall())
+    def get_records(self) -> None:
+        query = "select * from walks"
+        items = self.select(sql=query)
+        return items
+    
+    def delete_record(self, date: str) -> None:
+        query = f"delete from walks where date = '{date}'"
+        self.delete(sql=query)
