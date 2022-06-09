327d68aa1fe9c1850e3c909a3f8a3f6aeaffc001
k1-k0 <kmssrvk@gmail.com>
2022-06-09 22:19:55 +0300

    implement dirty main processing

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-
+import datetime as dt
 import argparse
-from ctypes import Union
+from pprint import pprint
 from typing import Optional, Tuple
-from utils import get_database_path
+from utils import get_database_path, date_from_str
 
 from database import Database
 from walkit import Walkit
-# TODO: Argparse
-# TODO: get env 
 
 
 def main():
@@ -19,19 +17,46 @@ def main():
     parser.add_argument('--total', '-t', action='store_true',
                         help='Returns the total number of meters traveled')
     parser.add_argument('--add', '-a', action='store', nargs='*',
-                        help='Returns the total number of meters traveled')
+                        help='Add new record to database')
+    parser.add_argument('--print', '-p', action='store_true',
+                        help='Returns all records in database')
+    parser.add_argument('--delete', '-d', action='store',
+                        help='Delete record by specified date')
     args = parser.parse_args()
 
+    # TODO: All arguments handlers to separate functions
     if args.total:
         print(walkit.total_metres())
     elif args.add:
-        values = parse_add_values(*args.add)
-        walkit.add_record(values)
+        metres, date = parse_add_values(*args.add)
+        if date:
+            walkit.add_record(metres, date)
+        else:
+            walkit.add_record(metres)
         print("Record has been written!")
+    elif args.print:
+        # TODO: Also must traveled metres for specified date
+        records = walkit.get_records()
+        if records:
+            pprint(records)
+        else:
+            print('There are no records in database.')
+    elif args.delete:
+        # TODO: Delete_record should pass datetime, not string
+        date = args.delete
+        walkit.delete_record(date)
+        print('Record has been removed!')
 
 
-def parse_add_values(metres: str, date: Optional[str]) -> Union[str, Tuple[str, str]]:
-    pass
+# TODO: Move to utils and add docstring
+def parse_add_values(metres: str,
+                     date: Optional[str] = None) -> Tuple[
+                         int, Optional[dt.datetime]]:
+    parsed_metres = int(metres)
+    if date:
+        parsed_date = date_from_str(date)
+        return (parsed_metres, parsed_date)
+    return (parsed_metres, None)
 
 
 if __name__ == '__main__':
--- walkit.py
+++ walkit.py
@@ -6,27 +6,32 @@ from database import Database
 
 # TODO: add logger
 class Walkit:
+    """Class for working with walk records"""
     def __init__(self, database: Database):
         self._database = database
 
     def add_record(self, metres: int = 0, 
                    date: Optional[dt.datetime] = None) -> None:
+        """Add record to database"""
         if not date:
-            date = dt.datetime.now().date()
+            date = dt.datetime.now()
 
         # TODO: Options if date already exists in table: Rewrite, Append, Nothing
         query = "insert into walks values (?, ?)"
-        self._database.insert(sql=query, values=(date, metres))
+        self._database.insert(sql=query, values=(date.date(), metres))
 
     def get_records(self) -> List[Tuple]:
+        """Returns all records from database"""
         query = "select * from walks"
         items = self._database.select(sql=query)
         return items
-    
+
     def delete_record(self, date: str) -> None:
+        """Delete record by given date"""
         query = f"delete from walks where date = '{date}'"
         self._database.delete(sql=query)
-    
+
     def total_metres(self) -> int:
+        """Returns total metres traveled"""
         items = self.get_records()
         return sum(metres for _, metres in items)
